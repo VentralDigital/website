fbe8017365e0d2273888a056f86c3be7fec95484
patrickd <patrick.d@ventral.org>
2024-09-09 16:33:16 +0200

    Fixing twitter banner

--- docs/theme.config.tsx
+++ docs/theme.config.tsx
@@ -1,5 +1,6 @@
 import type { DocsThemeConfig } from 'nextra-theme-docs'
 import { useConfig } from 'nextra-theme-docs'
+import { useRouter } from 'next/router'
 
 import Image from 'next/image';
 import logoImage from './public/logoflicker.svg'
@@ -28,25 +29,37 @@ const config: DocsThemeConfig = {
   }, */
   head: function useHead() {
     const { title } = useConfig()
+    const { asPath } = useRouter()
     return (
       <>
-        <meta name="msapplication-TileColor" content="#fff" />
-        <meta name="theme-color" content="#fff" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <meta httpEquiv="Content-Language" content="en" />
-        <meta name="description" content="Ventral Digital LLC is a research and consultancy firm currently specializing in Blockchain Security."/>
-        <meta name="og:description" content="Ventral Digital LLC is a research and consultancy firm currently specializing in Blockchain Security."/>
-        <meta name="twitter:card" content="summary_large_image" />
-        <meta name="twitter:image" content="/og.png" />
-        <meta name="twitter:site:domain" content="ventral.digital" />
-        <meta name="twitter:url" content="https://ventral.digital" />
-        <meta name="og:title" content={title ? title + ' • Ventral Digital' : 'Ventral Digital'}/>
-        <meta name="og:image" content="/og.png" />
-        <meta name="apple-mobile-web-app-title" content="Ventral Digital" />
         <link rel="icon" href="/favicon.svg" type="image/svg+xml" />
         <link rel="icon" href="/favicon.png" type="image/png" />
         <link rel="icon" href="/favicon-dark.svg" type="image/svg+xml" media="(prefers-color-scheme: dark)"/>
         <link rel="icon" href="/favicon-dark.png" type="image/png" media="(prefers-color-scheme: dark)"/>
+        <meta name="apple-mobile-web-app-title" content="Ventral Digital" />
+        <meta name="msapplication-TileColor" content="#fff" />
+        <meta name="theme-color" content="#fff" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta httpEquiv="Content-Language" content="en" />
+        <meta property="og:locale" content="en_US" />
+        <meta property="og:site_name" content="ventraldigital" />
+        <meta property="og:type" content="website" />
+        <meta name="description" content="Ventral Digital LLC is a research and consultancy firm specializing in Information Security and Privacy."/>
+        <meta property="og:description" content="Ventral Digital LLC is a research and consultancy firm specializing in Information Security and Privacy."/>
+        <meta name="twitter:description" content="Ventral Digital LLC is a research and consultancy firm specializing in Information Security and Privacy."/>
+        <meta name="twitter:site:domain" content="ventral.digital" />
+        <meta name="twitter:creator" content="@patrickd_de" />
+        <meta name="twitter:creator:id" content="493540838" />
+        <meta property="og:url" content={'https://ventral.digital' + asPath} />
+        <meta name="twitter:url" content={'https://ventral.digital' + asPath} />
+        <meta property="og:title" content={title ? title + ' • Ventral Digital' : 'Ventral Digital'}/>
+        <meta name="twitter:title" content={title ? title + ' • Ventral Digital' : 'Ventral Digital'}/>
+        <meta property="og:image:alt" content={title ? 'Banner for blog post on Ventral Digital titled ' + title : 'Banner for blog post on Ventral Digital'} />
+        <meta name="twitter:image:alt" content={title ? 'Banner for blog post on Ventral Digital titled ' + title : 'Banner for blog post on Ventral Digital'} />
+        <meta name="twitter:card" content="summary_large_image" />
+        /* Defaults, may be overridden by specific pages: */
+        <meta property="og:image" content="/og.png" />
+        <meta name="twitter:image" content="/og.png" />
       </>
     )
   },
